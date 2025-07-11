bec08c4705aca1e6501a9fb1f2e6cd85554fd9ed
Luke Tye <143744427+Lukewtye@users.noreply.github.com>
2025-07-10 22:01:37 -0600

    Updated text and corrected bugs

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+/* Things to fix 
+Make the links under the hamburger menu
+Make an animation for the hamburger menu smoother
+*/
+
